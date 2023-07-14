bf7fd896f7b2e161bcb8ddfefb97df0c098d45bb
[AI8/1반] 이도은 <delee1104@gmail.com>
2023-07-14 21:49:11 +0900

    fron_education

--- front/src/components/Education/Education.js
+++ front/src/components/Education/Education.js
@@ -1,51 +1,23 @@
-// import React, { useState } from "react";
-// import EducationCard from "./EducationCard";
-// import EducationEditForm from "./EducationEditForm";
-
-// function Education ({ education, setEducations, isEditable }) {
-//   // useState 훅을 통해 isEditing 상태를 생성함.
-//   const [isEditing, setIsEditing] = useState(false);
-//   return(
-//     <>
-//       {isEditing ? (
-//         <EducationEditForm
-//           currentEducation = {education}
-//           setEducations = {setEducations}
-//           setIsEditing = {setIsEditing}
-//         />
-//       ) : (
-//         <EducationCard
-//           education = {education}
-//           setIsEditing = {setIsEditing}
-//           isEditable = {isEditable}
-//         />
-//       )}
-//     </>
-//   );
-// }
-
-// export default Education;
-
 import React, { useState } from "react";
 import EducationCard from "./EducationCard";
 import EducationEditForm from "./EducationEditForm";
 
-function Education({ education, setEducations, isEditable }) {
-  //useState로 isEditing 상태를 생성함.
+function Education ({ education, setEducations, isEditable }) {
+  // useState 훅을 통해 isEditing 상태를 생성함.
   const [isEditing, setIsEditing] = useState(false);
-  return (
+  return(
     <>
       {isEditing ? (
         <EducationEditForm
-          currentEducation={education}
-          setEducations={setEducations}
-          setIsEditing={setIsEditing}
+          currentEducation = {education}
+          setEducations = {setEducations}
+          setIsEditing = {setIsEditing}
         />
       ) : (
         <EducationCard
-          education={education}
-          isEditable={isEditable}
-          setIsEditing={setIsEditing}
+          education = {education}
+          setIsEditing = {setIsEditing}
+          isEditable = {isEditable}
         />
       )}
     </>
@@ -54,4 +26,32 @@ function Education({ education, setEducations, isEditable }) {
 
 export default Education;
 
-//git merge
+// import React, { useState } from "react";
+// import EducationCard from "./EducationCard";
+// import EducationEditForm from "./EducationEditForm";
+
+// function Education({ education, setEducations, isEditable }) {
+//   //useState로 isEditing 상태를 생성함.
+//   const [isEditing, setIsEditing] = useState(false);
+//   return (
+//     <>
+//       {isEditing ? (
+//         <EducationEditForm
+//           currentEducation={education}
+//           setEducations={setEducations}
+//           setIsEditing={setIsEditing}
+//         />
+//       ) : (
+//         <EducationCard
+//           education={education}
+//           isEditable={isEditable}
+//           setIsEditing={setIsEditing}
+//         />
+//       )}
+//     </>
+//   );
+// }
+
+// export default Education;
+
+// //git merge
--- front/src/components/Education/EducationCard.js
+++ front/src/components/Education/EducationCard.js
@@ -1,33 +1,3 @@
-// // import { Card, Button, Row, Col } from "react-bootstrap";
-
-// // function EducationCard({ education, isEditable, setIsEditing }) {
-// //   return (
-// //     <Card.Text>
-// //       <Row className="align-items-center">
-// //         <Col>
-// //           <span>{education.title}</span>
-// //           <br />
-// //           <span className="text-muted">{education.description}</span>
-// //         </Col>
-// //         {isEditable && (
-// //           <Col xs lg="1">
-// //             <Button
-// //               variant="outline-info"
-// //               size="sm"
-// //               onClick={() => setIsEditing((prev) => !prev)}
-// //               className="mr-3"
-// //             >
-// //               편집
-// //             </Button>
-// //           </Col>
-// //         )}
-// //       </Row>
-// //     </Card.Text>
-// //   );
-// // }
-
-// // export default EducationCard;
-
 import { Card, Button, Row, Col } from "react-bootstrap";
 
 function EducationCard({ education, isEditable, setIsEditing }) {
@@ -57,4 +27,34 @@ function EducationCard({ education, isEditable, setIsEditing }) {
 }
 
 export default EducationCard;
+
+// import { Card, Button, Row, Col } from "react-bootstrap";
+
+// function EducationCard({ education, isEditable, setIsEditing }) {
+//   return (
+//     <Card.Text>
+//       <Row className="align-items-center">
+//         <Col>
+//           <span>{education.title}</span>
+//           <br />
+//           <span className="text-muted">{education.description}</span>
+//         </Col>
+//         {isEditable && (
+//           <Col xs lg="1">
+//             <Button
+//               variant="outline-info"
+//               size="sm"
+//               onClick={() => setIsEditing((prev) => !prev)}
+//               className="mr-3"
+//             >
+//               편집
+//             </Button>
+//           </Col>
+//         )}
+//       </Row>
+//     </Card.Text>
+//   );
+// }
+
+// export default EducationCard;
 
