5cb52a76ad274b75e508fc4441d8ade9e35bb073
[AI8/1반] 박소현 <noticeme54@gmail.com>
2023-07-14 20:26:19 +0900

    merge test1

--- front/src/components/award/award.js
+++ front/src/components/award/award.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import AwardCard from "./AwardCard";
 import AwardEditForm from "./AwardEditForm";
 
+// 병합 테스트하기 
+
 function Award({ award, setAwards, isEditable }) {
   //useState로 isEditing 상태를 생성함.
   const [isEditing, setIsEditing] = useState(false);
