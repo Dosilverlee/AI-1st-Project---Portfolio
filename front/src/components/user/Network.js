f896f761162fbbeb58d57920722be94f2904593b
[AI8/1반] 박영현 <chanel0908@gmail.com>
2023-07-14 19:01:55 +0900

    feat: /user 페이지에서 네트워크 페이지에 자신의 user card가 나오지 않게 설정

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -12,15 +12,26 @@ function Network() {
   const userState = useContext(UserStateContext);
   // useState 훅을 통해 users 상태를 생성함.
   const [users, setUsers] = useState([]);
+  const [id, setId] = useState();
 
   useEffect(() => {
     // "userlist" 엔드포인트로 GET 요청을 하고, users를 response의 data로 세팅함.
     Api.get("userlist").then((res) => setUsers(res.data));
   }, [userState, navigate]);
 
+  useEffect(() => {
+    if (params.userId) {
+      // 만약 현재 URL이 "/users/:userId" 라면, 이 userId를 유저 id로 설정함.
+      setId(params.userId);
+    } else {
+      // 이외의 경우, 즉 URL이 "/" 라면, 전역 상태의 user.id를 유저 id로 설정함.
+      setId(userState.user.id);
+    }
+  }, [params, userState]);
+
   return (
     <Col>
-      {users.filter((user) => user.id !== params.userId).map((user) => (
+      {users.filter((user) => user.id !== id).map((user) => (
         <UserCard key={user.id} user={user} isNetwork />
       ))}
     </Col>
