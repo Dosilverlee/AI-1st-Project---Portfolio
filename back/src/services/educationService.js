79e7ac179033bfe59a7e8420c06c785d8988b556
정현수 <hs@jeonghyeonsuui-MacBookAir.local>
2023-07-18 14:14:03 +0900

    fix

--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -20,18 +20,10 @@ class educationService {
     return createdNewEducation;
   }
 
-<<<<<<< HEAD
   //userId로 해당 유저의 학력 내용 전부 가져오기
   static async getEducationsByUserId({ userId }) {
     const educationData = await Education.findEducationsByUserId(userId);
     if (educationData.length === 0) { //5개면 5개
-=======
-  // 학력 이력 가져오기
-  static async getEducationByUserId({ userId }) {
-    // userID로 학력 이력 가져오기
-    const educationData = await Education.findByUserId(userId);
-    if (educationData.length == 0) {
->>>>>>> 927ab57f1bc7d4421f072a1a134eeef5df332dae
       return [];
     }
 
@@ -65,6 +57,7 @@ class educationService {
   static async deleteEducation({ id }) {
     const educationData= await Education.findEducationByEducationId(id);
   
+
     if (educationData === 0) {
       throw new Error("삭제할 이력이 없습니다.");
     }
@@ -73,4 +66,68 @@ class educationService {
   }
 }
 
-export { educationService };
+export { educationService };import { Education } from "../db/models/Educations"; 
+
+class EducationService {
+  //학력 이력 추가
+  static async addEducation({ userId, title, description }) {
+    const EducationData = await Education.findByUserId(userId);
+    if (EducationData.length > 1) {
+      const errorMessage =
+        "이미 등록된 수상이력입니다.";
+      return { errorMessage };
+    }
+
+    const newEducation = { userId, title, description };
+
+    // db에 저장
+    const createdNewEducation = await Education.create(newEducation);
+
+    return createdNewEducation;
+  }
+
+  // 학력 이력 가져오기
+  static async getEducationByUserId({ userId }) {
+    // userID로 학력 이력 가져오기
+    const educationData = await Education.findByUserId(userId);
+    if (educationData.length == 0) {
+      return [];
+    }
+
+    // 모든 학력 이력을 배열로 변환
+    const educationDataResult = educationData.map(education => ({
+      id: education._id,
+      userId: education.userId,
+      title: education.title,
+      description: education.description,
+    }));
+
+    return educationDataResult;
+  }
+
+  // 학력 이력 수정하기
+  static async setAward({ id, toUpdate }) {
+    let educationData = await Education.findById(id);
+
+    if (educationData === 0) {
+      const errorMessage = "학력 이력이 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    const updatedAward = await Award.update(id, toUpdate);
+    return updatedAward;
+  }
+
+  // 수상 이력 삭제하기
+  static async deleteAward({ id }) {
+    let awardData = await Award.findById(id);
+  
+    if (awardData === 0) {
+      throw new Error("삭제할 이력이 없습니다.");
+    }
+    const deletedEducation = await Education.findByIdAndRemove(id);
+    return deletedEducation;
+  }
+}
+
+export { EducationService };
