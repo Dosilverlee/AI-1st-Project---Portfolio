763afb5c05bc9a8ea3508d0fcf4531c4a71a566d
[AI8/1반] 김용우 <mixsam.taurusium@gmail.com>
2023-07-19 16:27:09 +0900

    fix:login_required 누락 수정

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -5,7 +5,7 @@ import { awardController } from "../controller/awardController";
 const awardRouter = Router();
 
 awardRouter.get('/awards/:userId', awardController.getAwardByUserId);
-awardRouter.post('/awards/:userId', awardController.addAward);
+awardRouter.post('/awards/:userId', login_required, awardController.addAward);
 awardRouter.put('/awards/:userId/:awardId', login_required, awardController.setAward);
 awardRouter.delete('/awards/:userId/:awardId', login_required, awardController.deleteAward);
 
