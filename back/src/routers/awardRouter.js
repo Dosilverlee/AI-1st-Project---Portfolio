9c49655051bd8b17c4fd4d6346534ba90973dc7b
[AI8/1반] 김용우 <mixsam.taurusium@gmail.com>
2023-07-20 15:32:15 +0900

    test:PUT, DELETE 오류확인을 위한 임시수정

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -5,8 +5,8 @@ import { awardController } from "../controller/awardController";
 const awardRouter = Router();
 
 awardRouter.get('/awards/:userId', awardController.getAwardByUserId);
-awardRouter.post('/awards/:userId', login_required, awardController.addAward);
-awardRouter.put('/awards/:userId/:awardId', login_required, awardController.setAward);
-awardRouter.delete('/awards/:userId/:awardId', login_required, awardController.deleteAward);
+awardRouter.post('/awards/:userId', awardController.addAward);
+awardRouter.put('/awards/:userId/:awardId', awardController.setAward);
+awardRouter.delete('/awards/:userId/:awardId', awardController.deleteAward);
 
 export { awardRouter };
