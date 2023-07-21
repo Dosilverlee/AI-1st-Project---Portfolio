b5572b220a8c70d512119d7a9e9be3b99f1c8170
[AI8/1반] 박소현 <noticeme54@gmail.com>
2023-07-21 18:05:21 +0900

    camera image

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -4,7 +4,7 @@ import { Nav } from "react-bootstrap";
 import { UserStateContext, DispatchContext } from "../App";
 import Logo from "../assets/logo.png";
 import Rabbit from "../assets/rabbit.png";
-import shake from "./shake.css"
+import shake from "./shake.css";
 
 function Header() {
   const navigate = useNavigate();
@@ -46,22 +46,72 @@ function Header() {
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div style={{ width: "1128px", height: "134px" }}>
           <div style={{ display: "inline-block", marginLeft: "20px" }}>
-            <a href="/"><img style={{ height: "33.5px" }} src={Logo} /></a>
-            <a href="/"><img style={{ marginLeft: "15px", height: "134px" }} src={Rabbit}/></a>
+            <a href="/">
+              <img style={{ height: "33.5px" }} src={Logo} />
+            </a>
+            <a href="/">
+              <img
+                style={{ marginLeft: "15px", height: "134px" }}
+                src={Rabbit}
+              />
+            </a>
           </div>
           {isLogin && (
-          <div style={{ display: "inline-block", float: "right" }}>
-            <div style={{ display: "inline-block",  marginTop: "20px"}}>
-              <div style={{ display:"flex", justifyContent: "center", marginBottom:"5px"}}>
-                <img style={{ width: "4rem", height: "4rem", borderRadius: "100%"}} src={profileImgSource}/>
+            <div style={{ display: "inline-block", float: "right" }}>
+              <div style={{ display: "inline-block", marginTop: "20px" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginBottom: "5px",
+                  }}
+                >
+                  <img
+                    style={{
+                      width: "4rem",
+                      height: "4rem",
+                      borderRadius: "100%",
+                    }}
+                    src={profileImgSource}
+                  />
+                </div>
+                <div>
+                  <Nav.Link
+                    style={{
+                      padding: "8px 16px",
+                      display: "inline-flex",
+                      color: "#6700e6",
+                      fontWeight: "bold",
+                    }}
+                    onClick={() => navigate("/")}
+                  >
+                    나의 페이지
+                  </Nav.Link>
+                  <Nav.Link
+                    style={{
+                      padding: "8px 16px",
+                      display: "inline-flex",
+                      color: "#6700e6",
+                      fontWeight: "bold",
+                    }}
+                    onClick={logout}
+                  >
+                    로그아웃
+                  </Nav.Link>
+                </div>
               </div>
-              <div>
-                <Nav.Link style={{padding: "8px 16px", display: "inline-flex", color: "#6700e6", fontWeight: "bold"}} onClick={() => navigate("/")}>나의 페이지</Nav.Link>
-                <Nav.Link style={{ padding: "8px 16px", display: "inline-flex", color: "#6700e6", fontWeight: "bold"}} onClick={logout}>로그아웃</Nav.Link>
-              </div>
+              <img
+                className="shake"
+                style={{
+                  display: "inline-block",
+                  width: "50px",
+                  height: "50px",
+                }}
+                src="https://cdn-icons-png.flaticon.com/512/1514/1514935.png"
+                alt="carrot"
+              />
             </div>
-            <img className="shake" style={{ display:"inline-block", width: "50px", height: "50px"}} src="https://cdn-icons-png.flaticon.com/512/1514/1514935.png" alt="carrot"/>
-          </div>)}
+          )}
         </div>
       </div>
     </div>
